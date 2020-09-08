2afa9e444015b011b48d4ac39838d4fe67649212
Evan Elias Young <evanyoung762@gmail.com>
2020-09-08 11:57:11 -0500

    :sparkles: Added a build file cleaner

---
+++ clean.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+###
+# @file      clean.sh
+# @brief     Cleans LaTex build scraps.
+#
+# @author    Evan Elias Young
+# @date      2020-09-08
+# @date      2020-09-08
+# @copyright Copyright 2020 Evan Elias Young. All rights reserved.
+###
+
+rm -rf *.out *.log *.fls *.fdb_latexmk *.synctex* *.pdf *.aux
